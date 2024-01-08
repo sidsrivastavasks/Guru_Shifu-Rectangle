a1510ecb25690e17e342b8d1f57e5d427c4b9663
Siddhant Srivastava <sidsrivastavasks@gmail.com>
2024-01-08 11:09:19 +0000

    Add. Perimeter calculation method

--- src/main/java/com/thoughtworks/rectangle/Rectangle.java
+++ src/main/java/com/thoughtworks/rectangle/Rectangle.java
@@ -1,15 +1,19 @@
 package com.thoughtworks.rectangle;
 
 public class Rectangle {
-    private final int length;
-    private final int breadth;
+    private final double length;
+    private final double breadth;
 
-    Rectangle(int length, int breadth){
+    Rectangle(double length, double breadth){
         this.length=length;
         this.breadth=breadth;
     }
 
-    public int area(){
+    public double area(){
         return length * breadth;
+    }
+
+    public double perimeter(){
+        return 2*(length + breadth);
     }
 }
--- src/test/java/com/thoughtworks/rectangle/RectangleTest.java
+++ src/test/java/com/thoughtworks/rectangle/RectangleTest.java
@@ -4,18 +4,32 @@ import org.junit.jupiter.api.Test;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.closeTo;
 
 
 public class RectangleTest{
     @Test
-    void shouldReturnAreaOf8WhenLengthIs2AndBredthIs4(){
-        int length = 2;
-        int breadth = 4;
-        int actual = 8;
+    void shouldReturnAreaWhenLengthAndBreadthIsGiven(){
+        double length = 2.0;
+        double breadth = 4.0;
+        double actual = 8.0;
 
         Rectangle rectangleInst = new Rectangle(length, breadth);
-        int expected = rectangleInst.area();
+        double expected = rectangleInst.area();
 
-        assertThat(expected, equalTo(actual));
+        assertThat(expected, is(closeTo(actual, 0.01)));
+    }
+
+    @Test
+    void shouldReturnPerimeterWhenLengthAndBreadthIsGiven(){
+        double length = 2.0;
+        double breadth = 4.0;
+        double actual = 12.0;
+
+        Rectangle rectangleInst = new Rectangle(length, breadth);
+        double expected = rectangleInst.perimeter();
+
+        assertThat(expected, is(closeTo(actual, 0.01)));
     }
 }
